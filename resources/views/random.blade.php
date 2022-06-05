c0f7b14ad0c1673a70d4f6108ceef1ddd3f9ccce
danila <dkiryanov@accurateweb.ru>
2022-06-05 22:16:58 +0500

    first

--- resources/views/random.blade.php
+++ resources/views/random.blade.php
@@ -3,7 +3,7 @@
 
     <div class="row row-conformity row-centered">
         <div class="card" style="max-width:40rem; ">
-            <img src="http://localhost/storage/{{$image->image}}"  class="img-fluid" alt="{{$image->color_name}}">
+            <img src="{{request()->getSchemeAndHttpHost()}}/storage/{{$image->image}}"  class="img-fluid" alt="{{$image->color_name}}">
             <div class="card-body">
                 <h5 class="card-title">{{$image->color_name}}</h5>
             </div>
