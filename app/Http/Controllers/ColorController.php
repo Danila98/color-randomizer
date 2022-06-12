2742b0d87b3d225952e6be795dc09d828be39ac0
danila <dkiryanov@accurateweb.ru>
2022-06-12 20:17:05 +0500

    vue require

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -11,11 +11,11 @@ class ColorController extends Controller
 
     public function index()
     {
-        return view('index');
+        return view('layout');
     }
     public function getRandomColor()
     {
-        $random_color = Color::orderBy(DB::raw('RAND()'))->take(1)->first();
+        $random_color = Color::where('type', '=', 0)->orderBy(DB::raw('RAND()'))->take(1)->first();
         return view('random', ['image' => $random_color]);
 
     }
--- app/Models/Color.php
+++ app/Models/Color.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Color extends Model
 {
+    const TYPE_COLOR = 0;
+    const TYPE_DESIGN = 1;
+
     use HasFactory;
 
     protected $fillable = [
         'color_name',
         'image',
+        'type',
     ];
 }
